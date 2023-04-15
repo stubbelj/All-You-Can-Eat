b830d95df4800552570a9f3db7e1375609b25c75
Your Name <handle@email.com>
2023-04-15 01:15:30 -0700

    more initlevel wip
    horrible spaghetti code that needs refactoring + experimenting with level gen. mostly comitting to get this onto my desktop

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,7 +8,9 @@ public class GameManager : MonoBehaviour
     public static GameManager inst = null;
 
     public Player player;
+    public List<GameObject> roomPrefabs = new List<GameObject>();
 
+    int level = 0;
     System.Random r = new System.Random();
 
     void Awake() {
@@ -32,7 +34,7 @@ public class GameManager : MonoBehaviour
         
     }
 
-    void InitLevel(int level=0) {
+    void InitLevel() {
         //initialize the floor layout for the level
         //first, create a sparse tree where nodes represent composites (chunks of several rooms) 
         //leaf nodes are composites that end in unique rooms (rewards, bosses,  shops) others are the normal gameplay loop
@@ -41,19 +43,8 @@ public class GameManager : MonoBehaviour
         //connect non-leaf composites by a single hallway at their origins/endpoints
         (int, int)[] compositeQuantity = new (int, int)[] {(1, 1), (3, 1), (6, 2)};
         //number of composites to load for each level BESIDES the start room, of normal and special types
-        string[][] normalCompositeTypes = new string[][] {
-            new string[] {"enemySmall"},
-            new string[] {"enemySmall", "enemyLarge"},
-            new string[] {"enemySmall", "enemyLarge"}
-        };
-        //types of composites that can load on each level, VERY VAGUE types
-        string[][] specialCompositeTypes = new string[][] {
-            new string[] {"reward"},
-            new string[] {"reward"},
-            new string[] {"reward"}
-        };
-        //types of special composites that can load on each level, VERY VAGUE types
         Graph graph = new Graph();
+        //in this graph, node.adj[0] is the "previous" node in the normal node branch and node.adj[1] is the next one
         graph.root.data = "root";
         int i = compositeQuantity[level].Item1;
         Node curr = graph.root;
@@ -88,12 +79,132 @@ public class GameManager : MonoBehaviour
             curr = curr.adj[0];
         }*/
 
-        curr = graph.root;
-        while(true) {
-            if (curr.adj.Contains(new Node(newData: "end"))) {
-                //double check that this works
+        Vector3[] units = new Vector3[] { Vector3.up, Vector3.down, Vector3.left, Vector3.right};
+        List<((float, float), (float, float))> spawnedCompositeBounds = new List<((float, float), (float, float))>();
+        curr = graph.root.adj[0];
+        //initialize curr as first normal node
+        int debugCounter = 100;
+        while(debugCounter > 0) {
+            //traverse through nodes, generating them as you go
+
+            (List<GameObject>, ((float, float), (float, float))) currComposite = GenerateComposite(curr);
+            //generate current normal composite
+            while (OverlapTransforms(currComposite.Item2, spawnedCompositeBounds)) {
+                //check if newly spawned composite overlaps with any previous composites. if so, move it
+                int j = r.Next(0, units.Length);
+                foreach (GameObject room in currComposite.Item1) {
+                    //move each room in composite
+                    room.transform.position += units[j] * (j <= 1 ? room.GetComponent<SpriteRenderer>().sprite.bounds.size.y : room.GetComponent<SpriteRenderer>().sprite.bounds.size.x);
+                }
+            }
+            spawnedCompositeBounds.Add(currComposite.Item2);
+            //add current normal composite to composite bounds list
+            foreach(Node node in curr.adj) {
+                //generate composites adjacent to the normal composite
+                if (node.data != "normal") {
+                    //don't generate the next normal composite yet
+                    (List<GameObject>, ((float, float), (float, float))) adjComposite = GenerateComposite(node);
+                    while (OverlapTransforms(adjComposite.Item2, spawnedCompositeBounds)) {
+                    
+                        //check if newly spawned adj composite overlaps with any previous composites. if so, move it
+                        int j = r.Next(0, units.Length);
+                        foreach (GameObject room in adjComposite.Item1) {
+                            //move each room in composite
+                            room.transform.position += units[j] * (j <= 1 ? room.GetComponent<SpriteRenderer>().sprite.bounds.size.y : room.GetComponent<SpriteRenderer>().sprite.bounds.size.x);
+                        }
+                    }
+                    spawnedCompositeBounds.Add(adjComposite.Item2);
+                    //add current adj composite to composite bounds list
+                }
+            }
+            if (curr.data == "end") {
+                //stop traversing if at last node
                 break;
             }
+            curr = curr.adj[1];
+            //move to the next normal node
+            debugCounter--;
         }
+    }
+
+    (List<GameObject>, ((float, float), (float, float))) GenerateComposite(Node node) {
+        //generates a room composite, a series of 4 connected rooms
+        string[][] normalCompositeTypes = new string[][] {
+            new string[] {"enemySmall"},
+            new string[] {"enemySmall", "enemyLarge"},
+            new string[] {"enemySmall", "enemyLarge"}
+        };
+        //types of composites that can load on each level, VERY VAGUE types
+        string[][] specialCompositeTypes = new string[][] {
+            new string[] {"reward"},
+            new string[] {"reward"},
+            new string[] {"reward"}
+        };
+        //types of special composites that can load on each level, VERY VAGUE types
+        Dictionary<string, int[]> roomIndexes = new Dictionary<string, int[]>{
+            {"root", new int[] {0} },
+            {"end", new int[] {1} },
+            {"enemySmall", new int[] {2} },
+            {"enemyLarge", new int[] {3} },
+            {"reward", new int[] {4} }
+        };
+        List<string> roomSpawnList = new List<string>();
+        switch(node.data) {
+            case "root":
+                //just the starting room
+                roomSpawnList.Add("root");
+                break;
+            case "end":
+                //just the end room
+                roomSpawnList.Add("end");
+                break;
+            case "normal":
+                //4-5 normal rooms
+                for(int i = 0; i < 4; i++) {
+                    roomSpawnList.Add(normalCompositeTypes[level][r.Next(0, normalCompositeTypes[level].Length)]);
+                    //add a random composite of a valid type for that level
+                }
+                break;
+            case "special":
+                //4-5 normal rooms with a special room at the end
+                for(int i = 0; i < 4; i++) {
+                    roomSpawnList.Add(normalCompositeTypes[level][r.Next(0, normalCompositeTypes[level].Length)]);
+                }
+                roomSpawnList.Add(specialCompositeTypes[level][r.Next(0, specialCompositeTypes[level].Length)]);
+                break;
+        }
+        Vector3[] units = new Vector3[] {Vector3.up, Vector3.down, Vector3.left, Vector3.right};
+        List<GameObject> spawnedRooms = new List<GameObject>();
+        foreach(string roomType in roomSpawnList) {
+            GameObject newRoom = GameObject.Instantiate(roomPrefabs[roomIndexes[roomType][r.Next(0, roomIndexes[roomType].Length)]]);
+            while (OverlapTransforms(newRoom, spawnedRooms)) {
+            
+            //while overlapping with any of the rooms in the composite, keep shifting room
+                int i = r.Next(0, units.Length);
+                newRoom.transform.position += units[i] * (i <= 1 ? newRoom.GetComponent<SpriteRenderer>().bounds.size.y : newRoom.GetComponent<SpriteRenderer>().bounds.size.x);
+            }
+
+            spawnedRooms.Add(newRoom);
+        }
+
+        List<GameObject> roomsListVal = new List<GameObject>();
+        ((float, float), (float, float)) boundsVal = ((roomsListVal[0].transform.position.x, roomsListVal[0].transform.position.x), (roomsListVal[0].transform.position.y, roomsListVal[0].transform.position.y));
+        foreach(GameObject obj in spawnedRooms) {
+            roomsListVal.Add(obj);
+            if (obj.transform.position.x - obj.GetComponent<SpriteRenderer>().bounds.size.x / 2 < boundsVal.Item1.Item1) {
+                boundsVal.Item1.Item1 = obj.transform.position.x - obj.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+            }
+            if (obj.transform.position.x + obj.GetComponent<SpriteRenderer>().bounds.size.x / 2 > boundsVal.Item1.Item2) {
+                boundsVal.Item1.Item2 = obj.transform.position.x + obj.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+            }
+            if (obj.transform.position.y - obj.GetComponent<SpriteRenderer>().bounds.size.y / 2 < boundsVal.Item2.Item1) {
+                boundsVal.Item2.Item1 = obj.transform.position.y - obj.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+            }
+            if (obj.transform.position.y + obj.GetComponent<SpriteRenderer>().bounds.size.y / 2 > boundsVal.Item2.Item2) {
+                boundsVal.Item1.Item2 = obj.transform.position.y + obj.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+            }
+        }
+        return (roomsListVal, boundsVal);
+
     }
 }
--- Assets/Utils.cs
+++ Assets/Utils.cs
@@ -15,4 +15,21 @@ public class Utils
             data = newData;
         }
     }
+
+    public static bool OverlapTransforms(((float, float), (float, float)) transform, List<((float, float), (float, float))> transformList) {
+        return true;
+    }
+
+    public static bool OverlapTransforms(GameObject gameObj, List<GameObject> gameObjList) {
+        ((float, float), (float, float)) transform = BoundsFromGameObject(gameObj);
+        List<((float, float), (float, float))> transformList = new List<((float, float), (float, float))>();
+        foreach(GameObject obj in gameObjList) {
+            transformList.Add(BoundsFromGameObject(obj));
+        }
+        return true;
+    }
+
+    public static ((float, float), (float, float)) BoundsFromGameObject(GameObject gameObj) {
+        return ((gameObj.transform.position.x - gameObj.GetComponent<SpriteRenderer>().bounds.size.x / 2, gameObj.transform.position.x + gameObj.GetComponent<SpriteRenderer>().bounds.size.x / 2), (gameObj.transform.position.y - gameObj.GetComponent<SpriteRenderer>().bounds.size.y / 2, gameObj.transform.position.y + gameObj.GetComponent<SpriteRenderer>().bounds.size.y / 2));
+    }
 }
